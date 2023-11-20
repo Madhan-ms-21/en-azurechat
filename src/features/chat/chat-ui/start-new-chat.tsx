a570b69186a69b69a5e704e004017de1688d2f24
M S Madhan Kumar <madkumar@IN-MADKUMAR-MBP.local>
2023-11-20 13:58:43 +0530

    logo size changed

--- src/features/chat/chat-ui/start-new-chat.tsx
+++ src/features/chat/chat-ui/start-new-chat.tsx
@@ -11,7 +11,7 @@ export const StartNewChat: FC<Prop> = (props) => {
   return (
     <div className="grid grid-cols-5 w-full items-center container mx-auto max-w-6xl justify-center h-full gap-9">
       <div className="col-span-4 gap-5 flex flex-col flex-1">
-        <Image width={400} height={250} alt="" src="/logo.png" />
+        <Image width={300} height={150} alt="" src="/logo.png" />
       
       <Card className="col-span-3 flex flex-col gap-5 p-5 ">
         <Typography variant="h4" className="text-primary">
